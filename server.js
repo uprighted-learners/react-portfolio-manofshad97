9c23ef73b2ed2dee6e9fde4fe8d5c2ebd93720a4
manofshad97 <shadman559@hotmail.com>
2021-07-13 17:59:51 -0400

    Portfolio Submission 3

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-const express = require('express')
+// initialize express
+const express = require("express");
+const app = express();
+// set default port number
+const port = process.env.PORT || 5000;
+// binding our server to a static directory
+app.use(express.static("./client/build"));
+// setting up route handler
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+// open a connection to listen for, and respond to requests
+app.listen(port, () => {
+  console.log("Listening on port: " + port)
+});
